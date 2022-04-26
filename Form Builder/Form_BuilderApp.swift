90d4ede7865aa1ac3805d848fc0884e72b75aeff
elichartnett <elichartnett@icloud.com>
2022-04-26 13:24:27 -0500

    Initial Commit

---
+++ Form Builder/Form_BuilderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Form_BuilderApp.swift
+//  Form Builder
+//
+//  Created by Eli Hartnett on 4/26/22.
+//
+
+import SwiftUI
+
+@main
+struct Form_BuilderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
