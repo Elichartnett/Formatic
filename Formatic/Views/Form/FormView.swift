110a74a0a5cc8d08a8f030f51bb2f6e2540a62ad
elichartnett <elichartnett@icloud.com>
2022-08-23 17:04:41 -0500

    Fix form background when no sections are present

--- Formatic/Views/Form/FormView.swift
+++ Formatic/Views/Form/FormView.swift
@@ -28,21 +28,25 @@ struct FormView: View {
             
             FormTitleView(form: form)
                 .padding(.horizontal)
-                .background(
-                    Color(uiColor: .systemGray6)
-                )
+                .background(Color(uiColor: .systemGray6))
+            
             
             // Display all section in form
             if !forPDF {
-                // List is used for user visibility due to built in support for swipe gestures and lazy loading. Can not be used for pdf because there is no finite height due to lazy loading. Without a finite height, the view is not rendered properly when exporting view as pdf.
-                List {
-                    ForEach(sections) { section in
-                        SwiftUI.Section {
-                            SectionView(section: section, locked: $form.locked, forPDF: forPDF)
-                        } header: {
-                            SectionTitleView(section: section, locked: $form.locked)
+                // List is used for user visibility due to built in support for swipe gestures and lazy loading. Can not be used for pdf because there is no finite height due to lazy loading. Without a finite height, the view is not rendered properly when exporting view as pdf. Can possibly replace this with ImageRenderer in ios 16
+                if sections.isEmpty {
+                    Color(uiColor: .systemGray6)
+                }
+                else {
+                    List {
+                        ForEach(sections) { section in
+                            SwiftUI.Section {
+                                SectionView(section: section, locked: $form.locked, forPDF: forPDF)
+                            } header: {
+                                SectionTitleView(section: section, locked: $form.locked)
+                            }
+                            .headerProminence(.increased)
                         }
-                        .headerProminence(.increased)
                     }
                 }
             }
@@ -50,23 +54,28 @@ struct FormView: View {
             else {
                 // ScrollView is used in place of list when exporting to pdf because it has a finite height so the full view can be rendered without scrolling. Modifiers added to make scrollview look like list
                 ScrollView {
-                    ForEach(sections) { section in
-                        SwiftUI.Section {
-                            VStack {
-                                SectionView(section: section, locked: $form.locked, forPDF: forPDF)
-                                    .padding(.top, 10)
+                    if sections.isEmpty {
+                        
+                    }
+                    else {
+                        ForEach(sections) { section in
+                            SwiftUI.Section {
+                                VStack {
+                                    SectionView(section: section, locked: $form.locked, forPDF: forPDF)
+                                        .padding(.top, 10)
+                                        .padding(.bottom, 10)
+                                        .padding(.horizontal, 20)
+                                        .background(.white)
+                                }
+                                .background(.white)
+                                .cornerRadius(10)
+                            } header: {
+                                SectionTitleView(section: section, locked: $form.locked)
+                                    .padding(.top)
                                     .padding(.bottom, 10)
-                                    .padding(.horizontal, 20)
-                                    .background(.white)
                             }
-                            .background(.white)
-                            .cornerRadius(10)
-                        } header: {
-                            SectionTitleView(section: section, locked: $form.locked)
-                                .padding(.top)
-                                .padding(.bottom, 10)
+                            .headerProminence(.increased)
                         }
-                        .headerProminence(.increased)
                     }
                 }
                 .padding()
@@ -74,7 +83,6 @@ struct FormView: View {
             
         }
         .navigationBarTitleDisplayMode(.inline)
-        .background(Color(uiColor: UIColor.systemGray6))
         .onDisappear {
             DataController.saveMOC()
         }
@@ -91,7 +99,7 @@ struct FormView_Previews: PreviewProvider {
                     }
                 })
                 .environmentObject(FormModel())
-                .environment(\.managedObjectContext, DataController.shared.container.viewContext)
+            //                .environment(\.managedObjectContext, DataController.shared.container.viewContext)
         }
         .navigationViewStyle(.stack)
     }
