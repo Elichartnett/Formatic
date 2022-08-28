ab3a285319eac76674dafae3597dde2485688d89
elichartnett <elichartnett@icloud.com>
2022-08-27 21:41:44 -0500

    Lottie

---
+++ Formatic/LottieView.swift
@@ -0,0 +1,44 @@
+//
+//  LottieView.swift
+//  Formatic
+//
+//  Created by Eli Hartnett on 8/27/22.
+//
+
+import SwiftUI
+import Lottie
+
+struct LottieView: UIViewRepresentable {
+    
+    var name: String
+    var loopMode: LottieLoopMode = .playOnce
+    
+    var animationView = AnimationView()
+    
+    func makeUIView(context: UIViewRepresentableContext<LottieView>) -> UIView {
+        let view = UIView(frame: .zero)
+        
+        animationView.animation = Animation.named(name)
+        animationView.contentMode = .scaleAspectFit
+        animationView.loopMode = .playOnce
+        animationView.play()
+        
+        animationView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(animationView)
+        
+        NSLayoutConstraint.activate([
+            animationView.heightAnchor.constraint(equalTo: view.heightAnchor),
+            animationView.widthAnchor.constraint(equalTo: view.widthAnchor)
+        ])
+        
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<LottieView>) {}
+}
+
+struct LottieView_Previews: PreviewProvider {
+    static var previews: some View {
+        LottieView(name: "logoAnimation")
+    }
+}
--- Formatic/Views/Home.swift
+++ Formatic/Views/Home.swift
@@ -10,10 +10,28 @@ import SwiftUI
 
 struct Home: View {
     
+    let timer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
+    @State var time = 0.0
+    @State var launchIsFinished = false
+    
     var body: some View {
         
         // List of all created forms
         FormListView()
+            .overlay {
+                LottieView(name: "logoAnimation")
+                    .background(Color.white)
+                    .onReceive(timer) { _ in
+                        time += 0.1
+                        if time >= 1.5 {
+                            withAnimation {
+                                launchIsFinished = true
+                            }
+                            timer.upstream.connect().cancel()
+                        }
+                    }
+                    .opacity(launchIsFinished == true ? 0 : 1)
+            }
     }
 }
 
