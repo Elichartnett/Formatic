e0086f0876183bc7baf1a76e9fecfa3bf1d49d99
Tanner Hess <89549856+TanHess@users.noreply.github.com>
2022-09-09 12:24:02 -0500

    Corrected CsvFormat function.

--- Formatic/Helpers/GlobalFunctions.swift
+++ Formatic/Helpers/GlobalFunctions.swift
@@ -10,8 +10,12 @@ import Foundation
 
 func CsvFormat(_ inString: String) -> String {
     var outString = ""
-    if inString.contains(",") {
-        outString = "\"" + inString.replacingOccurrences(of: "\"", with: "'") + "\""
+    if inString.contains(",") || inString.contains("\n") {
+        outString = "\"" + inString + "\""
+        if inString.contains("\""){
+            // In case user *specifcally* used normal quotes in their text
+            outString = outString.replacingOccurrences(of: "\"", with: "“")
+        }
     }
     else {
         outString = inString
