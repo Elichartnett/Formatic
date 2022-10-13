2ccb0fdd82c7f4fe5e235f8ce54d48cad00ab84c
elichartnett <elichartnett@icloud.com>
2022-10-13 09:36:50 -0500

    Specify error message for when forms request is empty vs when filtered forms request is empty

--- Formatic/Views/Form List Tab/FormListView.swift
+++ Formatic/Views/Form List Tab/FormListView.swift
@@ -11,6 +11,7 @@ import SwiftUI
 struct FormListView: View {
     
     @FetchRequest(sortDescriptors: [SortDescriptor(\.position)]) var forms: FetchedResults<Form>
+    @FetchRequest(sortDescriptors: [SortDescriptor(\.position)]) var filteredForms: FetchedResults<Form>
     @EnvironmentObject var formModel: FormModel
     @State var searchText = ""
     @State var showNewFormView = false
@@ -23,56 +24,66 @@ struct FormListView: View {
         
         NavigationView {
             
-            List {
-                
-                ForEach(forms) { form in
+            Group {
+                if !forms.isEmpty {
                     
-                    NavigationLink {
-                        FormEditorView(form: form)
-                    } label: {
-                        Text(form.title ?? "")
-                    }
-                    .swipeActions {
-                        Button {
-                            do {
-                                try formModel.deleteForm(position: Int(form.position))
-                            }
-                            catch {
-                                
-                            }
-                        } label: {
-                            Label("Delete", systemImage: "trash")
-                        }
-                        .tint(.red)
+                    List {
                         
-                        Button {
-                            do {
-                                try formModel.copyForm(form: form)
+                        ForEach(filteredForms) { form in
+                            
+                            NavigationLink {
+                                FormEditorView(form: form)
+                            } label: {
+                                Text(form.title ?? "")
                             }
-                            catch {
+                            .swipeActions {
+                                Button {
+                                    do {
+                                        try formModel.deleteForm(position: Int(form.position))
+                                    }
+                                    catch {
+                                        
+                                    }
+                                } label: {
+                                    Label("Delete", systemImage: "trash")
+                                }
+                                .tint(.red)
                                 
+                                Button {
+                                    do {
+                                        try formModel.copyForm(form: form)
+                                    }
+                                    catch {
+                                        
+                                    }
+                                } label: {
+                                    Label("Copy", systemImage: "doc.on.doc")
+                                }
+                                .tint(.blue)
                             }
-                        } label: {
-                            Label("Copy", systemImage: "doc.on.doc")
                         }
-                        .tint(.blue)
                     }
-                }
-            }
-            .searchable(text: $searchText, placement: .navigationBarDrawer)
-            .onChange(of: searchText, perform: { _ in
-                if searchText == "" {
-                    forms.nsPredicate = nil
+                    .searchable(text: $searchText, placement: .navigationBarDrawer)
+                    .onChange(of: searchText, perform: { _ in
+                        if searchText == "" {
+                            filteredForms.nsPredicate = nil
+                        }
+                        else {
+                            filteredForms.nsPredicate = NSPredicate(format: "title CONTAINS %@", searchText)
+                        }
+                    })
+                    .overlay {
+                        if filteredForms.isEmpty {
+                            Text("No results. Try searching for a different form title.")
+                                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                                .background(Color(uiColor: UIColor.systemGray6).ignoresSafeArea())
+                        }
+                    }
                 }
                 else {
-                    forms.nsPredicate = NSPredicate(format: "title CONTAINS %@", searchText)
-                }
-            })
-            .overlay {
-                if forms.isEmpty {
                     Text("Add a form to get started!")
                         .frame(maxWidth: .infinity, maxHeight: .infinity)
-                        .background(Color(uiColor: UIColor.systemGray6))
+                        .background(Color(uiColor: UIColor.systemGray6).ignoresSafeArea())
                 }
             }
             .navigationBarTitleDisplayMode(.inline)
